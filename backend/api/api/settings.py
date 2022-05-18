9f489cde549998efa20de7b2af79f1d8321051c2
DanBaDo <daniel.banobre.dopico@protonmail.com>
2022-05-18 23:33:05 +0200

    Inspections application installation

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Inspections app
+    'inspections',
     # Django REST framework application
     'rest_framework',
 ]
